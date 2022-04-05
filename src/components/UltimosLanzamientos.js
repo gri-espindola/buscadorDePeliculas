a90b7b85b570b49cf1bf84911c17b569e0225f2f
gri-espindola <griespindola@hotmail.com>
2022-04-04 21:08:57 -0300

    guardando cambios de estilado de ultimos lanzamientos en la master por error

--- src/components/UltimosLanzamientos.js
+++ src/components/UltimosLanzamientos.js
@@ -10,28 +10,31 @@ const peliculas = useFetchPeliculas("upcoming", "movie")
         <>
         <div>
             <h2 className="title is-2 is-spaced">Ultimos lanzamientos</h2>
-            {peliculas.map(pelicula => (
-                <article className="card-content">
-                    <div className="card-image">
-                        <figure className="image is-128x128">
-                            <img 
-                            src="https://www.hola.com/imagenes/mascotas/20180925130054/consejos-para-cuidar-a-un-gatito-recien-nacido-cs/0-601-526/cuidardgatito-t.jpg"
-                            alt= ""/>
-                        </figure>
-                    </div>
-                    <div className="card-media">
-                        <div className="media-content">
-                            <h3 key={pelicula.id}>{pelicula.title}</h3>
+            <div className="is-flex">
+                {peliculas.map(pelicula => (
+                    <article className="card-content">
+                        <div className="card-image">
+                            <figure className="image" width="auto" height="auto">
+                                <img 
+                                src={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`}
+                                alt= ""/>
+                            </figure>
                         </div>
-                        <div className="icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
-                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-                            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-                            </svg>
+                        <div className="card-media">
+                            <div className="media-content">
+                                <h3 key={pelicula.id}>{pelicula.title}</h3>
+                            </div>
+                            <div className="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                </svg>
+                            </div>
                         </div>
-                    </div>
-                </article>
-            ))}
+                    </article>
+                    
+                ))}
+            </div>
         </div>
         </>
     )
