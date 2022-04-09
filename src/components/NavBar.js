557aee22009ad04d365ff8017cda13d4a29e91b0
gri-espindola <griespindola@hotmail.com>
2022-04-08 23:48:52 -0300

    estilado de Nav, queda funcionalidad y menú hamburguesa

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,57 @@
 import {Link} from "react-router-dom"
 import 'bulma/css/bulma.min.css'
 import blackImg from './img-logo/black.png'
+import './NavBar.scss';
+
 
 const NavBar = () =>{
 
     return (
         <>
             <div>
-                <nav className="navbar is-black" aria-label="main navigation" role="navigation">
+                <nav className="navbar nav" aria-label="main navigation" role="navigation">
                     <div className="navbar-brand">
-                        <Link className="navbar-item" to="/home">
+                        <Link className="nav-img" to="/home">
                             <div width="150" height="80">
-                            <img src= {blackImg} alt="Logo de página"/>
+                            <img src= {blackImg} alt="Logo de página" className=""/>
                             </div>
                         </Link>
-                        <div className="navbar-burger" data-target="navbarExampleTransparentExample">
-                            <span>Últimos lanzamientos</span>
-                            <span>Populares</span>
-                            <span>Buscar</span>
+                    </div>
+
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a href="./home" class="navbar-link" aria-expanded="false">
+                        Menú
+                        </a>
+
+                        <div class="navbar-dropdown">
+                            <Link className="navbar-item nav-link" to="/home">
+                                Home
+                            </Link>
+                            <Link className="navbar-item nav-link" to="/ultimos-lanzamientos">
+                                Últimos lanzamientos
+                            </Link>
+                            <Link className="navbar-item nav-link" to="/populares">
+                                Populares
+                            </Link>
+                            <Link className="navbar-item nav-link" to="/buscador">
+                                Buscar
+                            </Link>
                         </div>
                     </div>
-                    <div id="navbarExampleTransparentExample" className="navbar-menu">
-                            <div className="navbar-start">
-                                <Link className="navbar-item" to="/home">
+
+                    <div className="navbar-menu tabs is-boxed">
+                                
+                            <div className="navbar-end">
+                                <Link className="navbar-item nav-link" to="/home">
                                     Home
                                 </Link>
-                                <Link className="navbar-item" to="/ultimos-lanzamientos">
+                                <Link className="navbar-item nav-link" to="/ultimos-lanzamientos">
                                     Últimos lanzamientos
                                 </Link>
-                                <Link className="navbar-item" to="/populares">
+                                <Link className="navbar-item nav-link" to="/populares">
                                     Populares
                                 </Link>
-                                <Link className="navbar-item" to="/buscador">
+                                <Link className="navbar-item nav-link" to="/buscador">
                                     Buscar
                                 </Link>
                             </div>
