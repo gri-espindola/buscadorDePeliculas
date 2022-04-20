9f0b36c229946903751e69cadc0e97e632418abf
gri-espindola <griespindola@hotmail.com>
2022-04-19 22:37:49 -0300

    andando ultimos lanzamientos y populares, no detalles

--- src/components/Populares.js
+++ src/components/Populares.js
@@ -1,34 +1,39 @@
-import useFetchPeliculas from "../hooks/useFetchPeliculas"
+import useFetchPeliculas from "../hooks/useFetchPeliculas";
+//import './Ultimos-lanzamientos.scss';
+import {Link} from "react-router-dom";
+
 
 
 const Populares =()=>{
 
-const peliculas = useFetchPeliculas("popular", "movie")
+const {peliculas} = useFetchPeliculas("popular", "movie")
 
 
     return (
         <>
-        <div>
-            <h2 className="title is-2 is-spaced">Populares</h2>
-            <div className="is-flex">
+        <div className="box-lanzamientos">
+            <h2 className="title is-2 is-spaced titulo">Populares</h2>
+            <div className="is-flex is-flex-wrap-wrap">
                 {peliculas.map(pelicula => (
-                    <article className="card-content">
+                    <article style={{width:"300px"}} className="card-lanzamientos">
                         <div className="card-image">
-                            <figure className="image" width="auto" height="auto">
+                            <figure className="image">
                                 <img 
+                                className="img-lanzamientos"
+                                style={{width:"100%", height:"420px"}}
                                 src={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`}
-                                alt= ""/>
+                                alt= {pelicula.title}/>
                             </figure>
                         </div>
-                        <div className="card-media">
-                            <div className="media-content">
+                        <div className="card-media card-descripcion">
+                            <div className="media-content title-lanzamientos">
                                 <h3 key={pelicula.id}>{pelicula.title}</h3>
                             </div>
-                            <div className="icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
-                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-                                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-                                </svg>
+                            <div className="icono-posicion">
+                            <div className="icon icon-lanzamientos">
+                                <Link className="fa fa-eye" to="/populares/:idDetallesPopulares" >
+                                </Link>
+                            </div>
                             </div>
                         </div>
                     </article>
@@ -46,7 +51,10 @@ const peliculas = useFetchPeliculas("popular", "movie")
   //{peliculas.map (pelicula => (
     //<h3 key={pelicula.id}>{pelicula.title}</h3>
       //))}
-
-
-
+      
+//<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+//<path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+//<path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/></svg>
+//
+//
 export default Populares
--- src/components/UltimosLanzamientos.js
+++ src/components/UltimosLanzamientos.js
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 
 const UltimosLanzamientos =()=>{
 
-const peliculas = useFetchPeliculas("upcoming", "movie")
+const {peliculas} = useFetchPeliculas("upcoming", "movie")
 
 
 
